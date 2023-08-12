7a4ae8182f02a858699870a386809d1bdaf174fd
1152958806@qq.com <1152958806@qq.com>
2023-08-12 15:02:29 +0800

    百度站长验证

---
+++ src/main/java/run/halo/sitepush/SiteHeadProcessor.java
@@ -0,0 +1,50 @@
+package run.halo.sitepush;
+
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IModel;
+import org.thymeleaf.model.IModelFactory;
+import org.thymeleaf.processor.element.IElementModelStructureHandler;
+import reactor.core.publisher.Mono;
+import run.halo.app.plugin.SettingFetcher;
+import run.halo.app.theme.dialect.TemplateHeadProcessor;
+
+
+@Component
+public class SiteHeadProcessor implements TemplateHeadProcessor {
+
+    private final SettingFetcher settingFetcher;
+
+    public SiteHeadProcessor(SettingFetcher settingFetcher) {
+        this.settingFetcher = settingFetcher;
+    }
+
+    @Override
+    public Mono<Void> process(ITemplateContext context, IModel model, IElementModelStructureHandler structureHandler) {
+        return settingFetcher.fetch("basic", BasicConfig.class)
+            .map(config -> {
+                final IModelFactory modelFactory = context.getModelFactory();
+                model.add(modelFactory.createText(siteHead(config)));
+                return Mono.empty();
+            }).orElse(Mono.empty()).then();
+    }
+
+    private String siteHead(BasicConfig config) {
+        String script = "";
+
+        //百度站长验证
+        if (StringUtils.isNotBlank(config.getSiteVerification())){
+            script = script + """
+                              <meta name="baidu-site-verification" content="%s">
+                """.formatted(config.getSiteVerification());
+        }
+        return script;
+    }
+
+    @Data
+    public static class BasicConfig {
+        String siteVerification;
+    }
+}
