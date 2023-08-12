79719bc5a460b83db924cf3898e2a780369e543b
stonewu <me@stonewu.com>
2023-08-12 13:32:11 +0800

    修复推送逻辑

--- src/main/java/run/halo/sitepush/strategy/BaiduPushStrategy.java
+++ src/main/java/run/halo/sitepush/strategy/BaiduPushStrategy.java
@@ -31,8 +31,8 @@ public class BaiduPushStrategy implements PushStrategy {
             if (GlobalCache.PUSH_CACHE.get(key) == null) {
                 GlobalCache.PUSH_CACHE.put(key, true);
                 String baiduPushUrl =
-                    String.format("http://data.zz.baidu.com/urls?token=%s",
-                        token);
+                    String.format("http://data.zz.baidu.com/urls?site=%s&token=%s",
+                        pageLink, token);
                 log.info("Pushing to baidu: {}", baiduPushUrl);
                 HttpResponse execute = HttpRequest.post(baiduPushUrl).form("site", pageLink).execute();
                 log.info("Pushing to baidu Result: {}", execute.body());
