7fb7123e1903d65cf3e437cbbb521a6b6f3a84b1
Erzbir <erzbir@mail.com>
2023-10-19 11:44:23 +0800

    将 Reconciler 回滚

--- src/main/java/com/stonewu/sitepush/reconciler/PostPushReconciler.java
+++ src/main/java/com/stonewu/sitepush/reconciler/PostPushReconciler.java
@@ -3,7 +3,7 @@ package com.stonewu.sitepush.reconciler;
 import com.stonewu.sitepush.DefaultSettingFetcher;
 import com.stonewu.sitepush.service.PushService;
 import com.stonewu.sitepush.setting.BasePushSetting;
-import java.time.Duration;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -14,6 +14,8 @@ import run.halo.app.extension.controller.Controller;
 import run.halo.app.extension.controller.ControllerBuilder;
 import run.halo.app.extension.controller.Reconciler;
 
+import java.time.Duration;
+
 /**
  * Reconciler for {@link Category}.
  *
@@ -21,27 +23,49 @@ import run.halo.app.extension.controller.Reconciler;
  * @since 2.0.0
  */
 @Component
+@AllArgsConstructor
 @Slf4j
-public class PostPushReconciler extends AbstractPushReconciler
-    implements Reconciler<Reconciler.Request> {
+public class PostPushReconciler implements Reconciler<Reconciler.Request> {
 
+    private final DefaultSettingFetcher settingFetcher;
 
-    public PostPushReconciler(DefaultSettingFetcher settingFetcher, PushService pushService,
-        ExtensionClient client) {
-        super(settingFetcher, pushService, client);
-    }
+    private final ExtensionClient client;
+
+    private PushService pushService;
 
     @Override
     public Result reconcile(Request request) {
-        return getResult(request);
+        return client.fetch(Post.class, request.name())
+                .map(post -> {
+                    BasePushSetting basePushSetting =
+                            settingFetcher.fetch(BasePushSetting.CONFIG_MAP_NAME, BasePushSetting.GROUP,
+                                    BasePushSetting.class).orElseGet(BasePushSetting::new);
+                    String siteUrl = basePushSetting.getSiteUrl();
+                    if (basePushSetting.getEnable() && StringUtils.hasText(siteUrl)) {
+                        if (post.isPublished()) {
+                            String slug = post.getSpec().getSlug();
+                            String permalink = post.getStatus().getPermalink();
+                            boolean allPush =
+                                    pushService.isAllPush(siteUrl, post.getKind() + ":" + slug, permalink);
+                            if(allPush){
+                                return Result.doNotRetry();
+                            }
+                        }
+                        // 未完整推送完成时，10分钟后重试
+                        return new Result(true, Duration.ofMinutes(10));
+                    }
+                    // 未启用插件时，忽略本次通知，10分钟后重试
+                    return new Result(true, Duration.ofMinutes(10));
+                })
+                .orElseGet(() -> new Result(false, null));
 
     }
 
     @Override
     public Controller setupWith(ControllerBuilder builder) {
         return builder
-            .extension(new Post())
-            .build();
+                .extension(new Post())
+                .build();
     }
 
 
