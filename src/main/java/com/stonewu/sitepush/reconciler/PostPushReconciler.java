342989f269d60596d5149bd5f5a64d349e29e4e9
Erzbir <erzbir@mail.com>
2024-01-09 20:10:21 +0800

    增加重试间隔设置并减少重复代码

--- src/main/java/com/stonewu/sitepush/reconciler/PostPushReconciler.java
+++ src/main/java/com/stonewu/sitepush/reconciler/PostPushReconciler.java
@@ -2,19 +2,15 @@ package com.stonewu.sitepush.reconciler;
 
 import com.stonewu.sitepush.DefaultSettingFetcher;
 import com.stonewu.sitepush.service.PushService;
-import com.stonewu.sitepush.setting.BasePushSetting;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import run.halo.app.core.extension.content.Category;
 import run.halo.app.core.extension.content.Post;
+import run.halo.app.extension.AbstractExtension;
 import run.halo.app.extension.ExtensionClient;
 import run.halo.app.extension.controller.Controller;
 import run.halo.app.extension.controller.ControllerBuilder;
 import run.halo.app.extension.controller.Reconciler;
-
-import java.time.Duration;
 
 /**
  * Reconciler for {@link Category}.
@@ -23,50 +19,24 @@ import java.time.Duration;
  * @since 2.0.0
  */
 @Component
-@AllArgsConstructor
 @Slf4j
-public class PostPushReconciler implements Reconciler<Reconciler.Request> {
+public class PostPushReconciler extends AbstractPushReconciler
+    implements Reconciler<Reconciler.Request> {
 
-    private final DefaultSettingFetcher settingFetcher;
-
-    private final ExtensionClient client;
-
-    private PushService pushService;
+    public PostPushReconciler(DefaultSettingFetcher settingFetcher, ExtensionClient client,
+        PushService pushService) {
+        super(settingFetcher, client, pushService);
+    }
 
     @Override
-    public Result reconcile(Request request) {
-        return client.fetch(Post.class, request.name())
-                .map(post -> {
-                    BasePushSetting basePushSetting =
-                            settingFetcher.fetch(BasePushSetting.CONFIG_MAP_NAME, BasePushSetting.GROUP,
-                                    BasePushSetting.class).orElseGet(BasePushSetting::new);
-                    String siteUrl = basePushSetting.getSiteUrl();
-                    if (basePushSetting.getEnable() && StringUtils.hasText(siteUrl)) {
-                        if (post.isPublished()) {
-                            String slug = post.getSpec().getSlug();
-                            String permalink = post.getStatus().getPermalink();
-                            boolean allPush =
-                                    pushService.isAllPush(siteUrl, post.getKind() + ":" + slug, permalink);
-                            if(allPush){
-                                return Result.doNotRetry();
-                            }
-                        }
-                        // 未完整推送完成时，10分钟后重试
-                        return new Result(true, Duration.ofMinutes(10));
-                    }
-                    // 未启用插件时，忽略本次通知，10分钟后重试
-                    return new Result(true, Duration.ofMinutes(10));
-                })
-                .orElseGet(() -> new Result(false, null));
-
+    public Class<? extends AbstractExtension> getExtension() {
+        return Post.class;
     }
 
     @Override
     public Controller setupWith(ControllerBuilder builder) {
         return builder
-                .extension(new Post())
-                .build();
+            .extension(new Post())
+            .build();
     }
-
-
 }
--- src/main/java/com/stonewu/sitepush/reconciler/SinglePagePushReconciler.java
+++ src/main/java/com/stonewu/sitepush/reconciler/SinglePagePushReconciler.java
@@ -1,19 +1,15 @@
 package com.stonewu.sitepush.reconciler;
 
 import com.stonewu.sitepush.DefaultSettingFetcher;
-import com.stonewu.sitepush.setting.BasePushSetting;
-import lombok.AllArgsConstructor;
+import com.stonewu.sitepush.service.PushService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import run.halo.app.core.extension.content.SinglePage;
+import run.halo.app.extension.AbstractExtension;
 import run.halo.app.extension.ExtensionClient;
 import run.halo.app.extension.controller.Controller;
 import run.halo.app.extension.controller.ControllerBuilder;
 import run.halo.app.extension.controller.Reconciler;
-import com.stonewu.sitepush.service.PushService;
-
-import java.time.Duration;
 
 /**
  * The {@link SinglePagePushReconciler} for route request to specific template <code>page
@@ -23,48 +19,24 @@ import java.time.Duration;
  * @since 2.0.0
  */
 @Component
-@AllArgsConstructor
 @Slf4j
-public class SinglePagePushReconciler implements Reconciler<Reconciler.Request> {
+public class SinglePagePushReconciler extends AbstractPushReconciler
+    implements Reconciler<Reconciler.Request> {
 
-    private final DefaultSettingFetcher settingFetcher;
-
-    private PushService pushService;
-
-    private final ExtensionClient client;
-
-    @Override
-    public Result reconcile(Request request) {
-        return client.fetch(SinglePage.class, request.name()).map(
-                        page -> {
-                            BasePushSetting basePushSetting =
-                                    settingFetcher.fetch(BasePushSetting.CONFIG_MAP_NAME, BasePushSetting.GROUP,
-                                            BasePushSetting.class).orElseGet(BasePushSetting::new);
-                            String siteUrl = basePushSetting.getSiteUrl();
-                            if (basePushSetting.getEnable() && StringUtils.hasText(siteUrl)) {
-                                if (page.isPublished()) {
-                                    String slug = page.getSpec().getSlug();
-                                    String permalink = page.getStatus().getPermalink();
-                                    boolean allPush =
-                                            pushService.isAllPush(siteUrl, page.getKind() + ":" + slug, permalink);
-                                    if(allPush){
-                                        return Result.doNotRetry();
-                                    }
-                                }
-                                // 未完整推送完成时，10分钟后重试
-                                return new Result(true, Duration.ofMinutes(10));
-                            }
-                            // 未启用插件时，忽略本次通知，10分钟后重试
-                            return new Result(true, Duration.ofMinutes(10));
-                        })
-                .orElseGet(() -> new Result(false, null));
+    public SinglePagePushReconciler(DefaultSettingFetcher settingFetcher, ExtensionClient client,
+        PushService pushService) {
+        super(settingFetcher, client, pushService);
     }
 
     @Override
     public Controller setupWith(ControllerBuilder builder) {
         return builder
-                .extension(new SinglePage())
-                .build();
+            .extension(new SinglePage())
+            .build();
     }
 
+    @Override
+    public Class<? extends AbstractExtension> getExtension() {
+        return SinglePage.class;
+    }
 }
