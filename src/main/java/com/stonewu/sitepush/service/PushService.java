c6441c98245211b488eb8f1c20488dc7e2fc12ff
Erzbir <erzbir@mail.com>
2024-01-11 02:20:26 +0800

    重构 api

--- src/main/java/com/stonewu/sitepush/service/PushService.java
+++ src/main/java/com/stonewu/sitepush/service/PushService.java
@@ -1,5 +1,9 @@
 package com.stonewu.sitepush.service;
 
+import com.stonewu.sitepush.strategy.PushStrategy;
+
 public interface PushService {
-    boolean isAllPush(String site, String slugKey, String permalink);
+    boolean pushUseAllStrategy(String siteUrl, String slugKey, String... permalinks);
+
+    boolean push(String siteUrl, PushStrategy pushStrategy, String slug, String... permalinks);
 }
